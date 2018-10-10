939af54486d82909ed179a041dc157e627c3f8cf
tamago0224 <bigtreesasuke200733@gmail.com>
2018-10-11 07:43:19 +0900

    replのアップデート

--- repl/repl.go
+++ repl/repl.go
@@ -4,7 +4,8 @@ import (
 	"bufio"
 	"fmt"
 	"github.com/tamago0224/monkey/lexer"
-	"github.com/tamago0224/monkey/token"
+	"github.com/tamago0224/monkey/parser"
+	//"github.com/tamago0224/monkey/token"
 	"io"
 )
 
@@ -24,10 +25,21 @@ func Start(in io.Reader, out io.Writer) {
 
 		line := scanner.Text()
 		l := lexer.New(line)
+		p := parser.New(l)
 
-		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-			fmt.Printf("%+v\n", tok)
+		program := p.ParseProgram()
+		if len(p.Errors()) != 0 {
+			printParserErrors(out, p.Errors())
+			continue
 		}
 
+		io.WriteString(out, program.String())
+		io.WriteString(out, "\n")
+	}
+}
+
+func printParserErrors(out io.Writer, errors []string) {
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
 	}
 }
