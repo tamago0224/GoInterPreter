282e361d38a6667cfa1da20f5132e5298fdf1bbc
tamago0224 <bigtreesasuke200733@gmail.com>
2018-10-18 23:08:23 +0900

    関数の評価

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -205,6 +205,62 @@ func TestLetStatements(t *testing.T) {
 	}
 }
 
+func TestFunctionObject(t *testing.T) {
+
+	input := "fn(x) { x + 2; };"
+
+	evaluated := testEval(input)
+	fn, ok := evaluated.(*object.Function)
+	if !ok {
+		t.Fatalf("object is not Function. got=%T (%+v)", evaluated, evaluated)
+	}
+
+	if len(fn.Parameters) != 1 {
+		t.Fatalf("function has wrong parameters. Parameters=%+v",
+			fn.Parameters)
+	}
+
+	if fn.Parameters[0].String() != "x" {
+		t.Fatalf("paramete is not 'x'. got=%q", fn.Parameters[0])
+	}
+
+	expectedBody := "(x + 2)"
+
+	if fn.Body.String() != expectedBody {
+		t.Fatalf("body is not %q. got=%q", expectedBody, fn.Body.String())
+	}
+}
+
+func TestFunctionApplication(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected int64
+	}{
+		{"let identity = fn(x){ x; }; identity(5);", 5},
+		{"let identity = fn(x) { return x; }; identity(10);", 10},
+		{"let double = fn(x) { x * 2; }; double(4);", 8},
+		{"let add = fn(x, y) { x + y; }; add(3, 4);", 7},
+		{"let add = fn(x, y) { x + y; }; add(4, add(6, 7));", 17},
+		{"fn(x) { x; }(4)", 4},
+	}
+
+	for _, tt := range tests {
+		testIntegerObject(t, testEval(tt.input), tt.expected)
+	}
+}
+
+func TestClosures(t *testing.T) {
+	input := `
+	let newAdder = fn(x) {
+		fn(y) { x + y };
+	};
+	let addTwo = newAdder(2);
+	addTwo(2);
+	`
+
+	testIntegerObject(t, testEval(input), 4)
+}
+
 func testEval(input string) object.Object {
 	l := lexer.New(input)
 	p := parser.New(l)
